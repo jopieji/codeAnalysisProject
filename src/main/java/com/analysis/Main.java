dba848ff7cc666369e344a695f9bb6f7a924e456
jopieji <jakeopie9@gmail.com>
2023-03-03 15:06:36 -0600

    pmd without config finished and amended notes

--- src/main/java/com/analysis/Main.java
+++ src/main/java/com/analysis/Main.java
@@ -11,22 +11,27 @@ public class Main {
         // eventually implement with keyword arguments
         // private repo to throw auth exception
 
-        // take input from user
+        // take input from user using Scanner
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter your url: ");
         String url = sc.next();
+        // closed scanner
+        sc.close();
 
         // instantiate Fetch object with user URL
         Fetch fetch = new Fetch(url);
+        // clone repository to local using Fetch class
         fetch.cloneRepository();
 
+        // get filepath from Fetch class
         File f = fetch.getOutFile();
 
-        // run PMD
+        // run PMD using Analyzer class, which picks out java
+        // files and analyzes them for code style
         Analyzer analyzer = new Analyzer(f);
         analyzer.runPMD();
 
         // maybe use JGit instead to show new directory created or contents
-        System.out.println("Done!");
+        System.out.println("Done! Check the pmd-report.csv file for output");
     }
 }
